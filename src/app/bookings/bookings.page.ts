08f98d6b63ef6693438e9e4df357f9458098cf61
Tomas-a21803655 <57908569+Tomas-a21803655@users.noreply.github.com>
2021-02-10 17:33:39 +0000

    Headers, booking page

--- src/app/bookings/bookings.page.ts
+++ src/app/bookings/bookings.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-bookings',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bookings.page.scss'],
 })
 export class BookingsPage implements OnInit {
+  private homeTripCards;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private router: Router, public db: AngularFirestore) {
   }
 
+  ngOnInit() {
+    this.getAllhomeTripCards().subscribe((data) => {
+      this.homeTripCards = data;
+    });
+  }
+
+  getAllhomeTripCards(): Observable<any> {
+    return this.db.collection<any>('homeTripCards').valueChanges();
+  }
 }
