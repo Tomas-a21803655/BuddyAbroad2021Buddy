12d278c5a0b8cce70f3c4b91c5a41d612709230d
Tomas-a21803655 <57908569+Tomas-a21803655@users.noreply.github.com>
2021-02-15 23:47:03 +0000

    base s

--- src/app/create/create.page.ts
+++ src/app/create/create.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NavController} from '@ionic/angular';
 
 @Component({
   selector: 'app-create',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatePage implements OnInit {
 
-  constructor() { }
+  constructor(private navCtrl:NavController) { }
 
   ngOnInit() {
+  }
+
+  goback() {
+    this.navCtrl.pop();
   }
 
 }
