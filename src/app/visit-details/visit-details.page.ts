f30f248eb91011d40921f1165183c3d91b65b748
Tomas-a21803655 <57908569+Tomas-a21803655@users.noreply.github.com>
2021-03-16 19:40:59 +0000

    list implemented, need to fix index loading

--- src/app/visit-details/visit-details.page.ts
+++ src/app/visit-details/visit-details.page.ts
@@ -1,32 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NavController} from '@ionic/angular';
 import {ActivatedRoute, Router} from '@angular/router';
 import {HomeTripCardsModel} from '../shared/homeTripCards.model';
 import {FireStorageService} from '../fire-storage.service';
-import {Observable} from 'rxjs';
+import {Subscription} from 'rxjs';
+import {AngularFirestore} from '@angular/fire/firestore';
 
 @Component({
-  selector: 'app-visit-details',
-  templateUrl: './visit-details.page.html',
-  styleUrls: ['./visit-details.page.scss'],
+    selector: 'app-visit-details',
+    templateUrl: './visit-details.page.html',
+    styleUrls: ['./visit-details.page.scss'],
 })
 export class VisitDetailsPage implements OnInit {
 
-  public trip: HomeTripCardsModel;
+    public trip: HomeTripCardsModel;
+    public allUsersEnrolled: any = [];
+    public allUsersEnrolledInfo: any = [];
+    public tripId: string = this.route.snapshot.paramMap.get('id');
 
 
-  constructor(private router: Router,private navCtrl:NavController,
-              public fireStorageService: FireStorageService,
-              private route: ActivatedRoute) { }
+    constructor(private router: Router, private navCtrl: NavController,
+                public fireStorageService: FireStorageService,
+                private route: ActivatedRoute, public db: AngularFirestore) {
+    }
 
-  ngOnInit() {
-    const tripId: string = this.route.snapshot.paramMap.get('id');
-    this.fireStorageService.getTripDetail(tripId).subscribe(trip => {
-      this.trip = trip;
-    });
-  }
+    ngOnInit() {
+        const tripId: string = this.route.snapshot.paramMap.get('id');
+        this.fireStorageService.getTripDetail(tripId).subscribe(trip => {
+            this.trip = trip;
+        });
+        this.db.collection('users').get()
+            .subscribe(querySnapshot => {
+                querySnapshot.forEach(doc => {
+                    this.getEnrolledUsers(doc.id);
+                });
+            });
 
-  goback() {
-    this.navCtrl.pop();
-  }
+    }
+
+    public getEnrolledUsers(targetUser): Subscription {
+        return this.db.collection('users').doc(targetUser).collection('buddyScheduledTrips').get()
+            .subscribe(querySnapshot => {
+                querySnapshot.forEach(doc => {
+                    const data = doc.data()
+                    if (data.orderedTripId === this.tripId) {
+                        this.getEnrolledUserInfo(data.orderedBy);
+                        this.allUsersEnrolled.push(doc.data());
+                    }
+                });
+            });
+    }
+
+    public getEnrolledUserInfo(targetUser): any {
+        this.db.collection('users').doc(targetUser).get()
+            .subscribe(doc => {
+                this.allUsersEnrolledInfo.push(doc.data());
+            });
+    }
+
+
+    goback() {
+        this.navCtrl.pop();
+    }
 }
